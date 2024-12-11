c5dce47e0aa6221deb5bbd4ea22248d88f3fb5de
Назар Усацкий <usackiy95@gmail.com>
2024-12-11 21:02:52 +0300

    начал собирать схему

--- Frontend/src/components/Scheme.vue
+++ Frontend/src/components/Scheme.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="scheme">
-    <RCD />
-    <QF_1 />
+    <div class="power__wrapp">
+      <div class="power-line"></div>
+      <div class="power-nodes__wrapp"></div>
+    </div>
   </div>
 </template>
-<script setup>
-import RCD from "./legend/RCD.vue"
-import QF_1 from "./legend/QF-1.vue"
-</script>
+<script setup></script>
 <style lang="scss" scoped>
 .scheme {
   position: absolute;
   left: 43mm; //     Позицианирование относительно левой таблицы  "Данные питающей сети"
   top: 4.2mm; //     Позицианирование относительно левой таблицы  "Данные питающей сети"
+}
+.power__wrapp {
+  max-width: 220mm;
+  display: flex;
+  flex-direction: column;
+}
+.power-line {
+  height: 1mm;
+  width: 225mm;
+  border: 0.5mm solid #000;
+}
+.power-nodes__wrapp {
+  display: flex;
+  flex-direction: row;
 }
 </style>
--- Frontend/src/components/legend/QF-1.vue
+++ Frontend/src/components/legend/QF-1.vue
@@ -1,24 +1,10 @@
 <template>
   <div class="QF-1__wrapper">
-    <div class="line-1">
-      <div class="parallelogram-2"></div>
-      <div class="rectangle"></div>
-      <div class="oval"></div>
-      <div class="arrow"></div>
-    </div>
+    <img src="../../assets/img/QF-0.svg" alt="" />
   </div>
 </template>
 <script setup></script>
 <style lang="scss" scoped>
 .QF-1__wrapper {
-  width: 12mm;
-  height: 12mm;
-  display: flex;
-  align-items: flex-start;
-  justify-content: center;
-}
-.line-1 {
-  height: 7mm;
-  border-left: 0.5mm solid #000;
 }
 </style>
