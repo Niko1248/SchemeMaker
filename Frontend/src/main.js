31471bdaf37cb71499e46111aefd8143e224f253
Niko1248 <Sirius04@bk.ru>
2024-12-12 14:21:23 +0300

    Почистил код

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,13 +1,11 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import './style.css'
-import './scss/global.scss'
-import './scss/size.scss'
-import './../src/assets/fonts/stylesheet.css'
+import { createApp } from "vue"
+import "./style.css"
+import "./scss/global.scss"
+import "./scss/size.scss"
+import "./../src/assets/fonts/stylesheet.css"
 
-import App from './App.vue'
+import App from "./App.vue"
 
 const app = createApp(App)
-const pinia = createPinia()
 
-app.use(pinia).mount('#app')
+app.mount("#app")
